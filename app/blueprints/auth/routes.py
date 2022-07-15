58c406853090728f41411c1083aeeb69ce5b89e9
MeilaniNishimura <meilaninishimura@gmail.com>
2022-07-14 20:58:06 -0500

    "attempting to add register form"

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,10 +1,24 @@
-from flask import render_template
+from flask import render_template, request
 from . import bp as app
+from app.blueprints.main.models import User
+from app import db
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        new_user = User(
+            email = request.form['emailInput'],
+            password = request.form['passwordInput'],
+            username=request.form['usernameInput'],
+            first_name= request.form['first_nameInput'],
+            last_name = request.form['last_nameInput']
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return request.form
+    else:
+        return render_template("register.html")
